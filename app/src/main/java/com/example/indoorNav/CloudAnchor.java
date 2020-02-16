02d3a5c26cf055e8e3a64c74d33550bb807bc99b
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-02-16 12:57:55 +0530

    cloud management

--- app/src/main/java/com/example/indoorNav/CloudAnchor.java
+++ app/src/main/java/com/example/indoorNav/CloudAnchor.java
@@ -1,6 +1,14 @@
 package com.example.indoorNav;
 
-public class CloudAnchor {
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
 
+public class CloudAnchor extends IndoorNavMainActivity{
 
+//    @Override
+//    protected Config getSessionConfiguration(Session session) {
+//        Config config = super.getSessionConfiguration(session);
+//        config.setCloudAnchorMode(Config.CloudAnchorMode.ENABLED);
+//        return config;
+//    }
 }
